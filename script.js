278e18a796b5a26da57cf9b440185763dbbd577c
JeffProGamer <168239949+JeffProGamer@users.noreply.github.com>
2024-09-05 17:41:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = 'game.zip'; // URL to the game file, or path to the game file in your project
-    link.download = 'game.zip'; // Name of the file to download
+    link.href = 'game.bat'; // URL to the game file, or path to the game file in your project
+    link.download = 'game.bat'; // Name of the file to download
     link.click();
 });
